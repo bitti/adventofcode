4b9fc0967af416d972ab136f4cce2849a85127dc
David Ongaro <david.ongaro@hamburg.de>
2015-12-10 00:59:26 -0800

    First part of day 1 solution
    A little obfuscated to make it more interesting...

---
+++ day1.jl
@@ -0,0 +1,14 @@
+# Fetch input
+
+bytes = readbytes(STDIN)
+
+# In order to understand this you have to know the bit representation of the ASCII codes
+# of '(' and ')'. But in case you forgot these you can ask julia:
+#
+# julia> bits(int8('('))
+# "00101000"
+#
+# julia> bits(int8(')'))
+# "00101001"
+
+println(reduce(+, int8(bytes&1 * -2 + 1)))
