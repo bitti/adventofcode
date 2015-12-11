f7afdc9579255963cf0642266c5c90f514c6afab
David Ongaro <david.ongaro@hamburg.de>
2015-12-11 10:43:59 -0800

    Solution day 6 part 1

---
+++ day6-part1.jl
@@ -0,0 +1,15 @@
+const format = r"(turn )?(on|off|toggle) (\d+),(\d+) \w+ (\d+),(\d+)"
+
+lights = falses(1000, 1000)
+
+for instruction in readlines(STDIN)
+    _, action, x1, y1, x2, y2 = match(format, instruction).captures
+
+    # 1-index based
+    x1, x2, y1, y2 = map(x -> int(x) + 1, [x1 x2 y1 y2])
+
+    # NOTE: I also tried out flipbits! but that doesn't mutate subarrays
+    lights[x1:x2, y1:y2] = action == "toggle" ? ~lights[x1:x2, y1:y2] : action == "on"
+end
+
+println(sum(lights))
