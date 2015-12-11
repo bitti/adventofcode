840fe264076a96fd5cc5f64d207a55126266b980
David Ongaro <david.ongaro@hamburg.de>
2015-12-10 21:41:45 -0800

    Solution for day 3 part 2

---
+++ day3-part2.jl
@@ -0,0 +1,20 @@
+# Starting positions
+p1 = p2 = [0 0]
+
+directions = [
+    '^' => [ 1  0],
+    '>' => [ 0  1],
+    'v' => [-1  0],
+    '<' => [ 0 -1]]
+
+# Starting house (wrap position inside a tuple to prevent destructuring)
+visited = Set((p1,))
+
+for d in readall(STDIN)
+    push!(visited, p1 += directions[d])
+
+    # Swap roles
+    p1, p2 = p2, p1
+end
+
+println(length(visited))
