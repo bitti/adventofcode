105edf66ba4aae71785614ffc8acde4671bdb8b0
David Ongaro <david.ongaro@hamburg.de>
2015-12-12 03:04:11 -0800

    Solution day 7 part 2

---
+++ 07/day7-part2.jl
@@ -0,0 +1,43 @@
+const format = r"(\w+|NOT (\w+)|(\w+) (AND|OR|LSHIFT|RSHIFT) (\w+)) -> (\w+)"
+
+connections = Dict()
+
+const binary_operations = Dict(
+    "AND" => (x, y) -> x & y,
+    "OR" => (x, y) -> x | y,
+    "RSHIFT" => (x, y) -> x >>> y,
+    "LSHIFT" => (x, y) -> x << y)
+
+function signal(input)
+    if isdigit(input)
+        () -> parse(UInt16, input)
+    else
+        () -> begin
+            m = connections[input]()
+
+            # Memoize the result
+            connections[input] = () -> m
+            m
+        end
+    end
+end
+
+for instruction in readlines(STDIN)
+    input, inv_input, input1, op, input2, output = match(format, instruction).captures
+
+    connections[output] =
+        if inv_input != nothing
+            () -> ~signal(inv_input)()
+        elseif input1 != nothing
+            () -> binary_operations[op](signal(input1)(), signal(input2)())
+        else
+            signal(input)
+        end
+end
+
+copied_connections = copy(connections)
+
+b = signal("a")()
+connections = copied_connections
+connections["b"] = () -> b
+println(signal("a")())
