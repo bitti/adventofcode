c0cb83973f34e7a90fbf5c3c25859348c3396329
David Ongaro <david.ongaro@hamburg.de>
2015-12-13 01:50:02 -0800

    Solution for day 13 part 1

---
+++ 13/day13-part1.jl
@@ -0,0 +1,18 @@
+const format = r"(\w+) would (\w+) (\w+) .* (\w+)"
+const happiness = Dict{Pair,Int}()
+const attendees = Set{AbstractString}()
+
+for line in readlines(STDIN)
+    name1, dir, diff, name2 = match(format, line).captures
+    happiness[name1 => name2] = (dir == "gain" ? 1 : -1) * parse(Int, diff)
+    push!(attendees, name1)
+end
+
+const total = length(attendees)
+
+println(maximum(permutations(collect(attendees))) do arrangement
+    sum(1:total) do i
+        happiness[arrangement[i] => arrangement[mod1(i + 1, total)]] +
+        happiness[arrangement[i] => arrangement[mod1(i - 1, total)]]
+    end
+end)
