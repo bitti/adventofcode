a5a5d11b8173a1cde156556d6d89180f82759b6d
David Ongaro <david.ongaro@hamburg.de>
2015-12-12 18:26:02 -0800

    Solution to day 9 part 1
    Of course the next visited location is always next_loc. Thanks to
    null_ptr on #julia@freenode for spotting this

--- 09/day9-part1.jl
+++ 09/day9-part1.jl
@@ -1,10 +1,11 @@
 const format = r"(\w+) to (\w+) = (\w+)"
-const distances = Dict{Pair{SubString{UTF8String}, SubString{UTF8String}}, UInt}()
+const distances = Dict{Pair, UInt}()
 const locations = Set{AbstractString}()
 
 for line in readlines(STDIN)
     loc1, loc2, distance = match(format, line).captures
     distances[loc1 => loc2] = distances[loc2 => loc1] = parse(UInt, distance)
+    distances[nothing => loc1] = distances[nothing => loc2] = 0
     push!(locations, loc1, loc2)
 end
 
@@ -16,11 +17,7 @@ function find_min(location, remaining_locations, distance)
     minimum(map(remaining_locations) do next_loc
         remaining = copy(remaining_locations)
         pop!(remaining, next_loc)
-        if location == nothing
-            find_min(next_loc, remaining, distance)
-        else
-            find_min(location, remaining, distance + distances[location => next_loc])
-        end
+        find_min(next_loc, remaining, distance + distances[location => next_loc])
     end)
 end
 
