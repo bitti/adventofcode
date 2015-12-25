1de2a2773a13f8727ba2122ba4dadc6b31ef03c6
David Ongaro <david.ongaro@hamburg.de>
2015-12-25 00:54:47 -0800

    Solution for day 24
    It turns out the checks if a partitioning with the lowest 'quantum
    entanglement' is not necessary with the given inputs. I left it in
    anyway, in order to allow more general inputs.
    
    Note that this still assumes that there exists at least one partitioning
    for with the minimum length group (but that is a much weaker assumption
    that it also exists for the lowest quantum entanglement group).

---
+++ 24/day24-part2.jl
@@ -0,0 +1,45 @@
+using Iterators
+
+const weights = reverse(sort(map(parse, readlines(STDIN))))
+const g = sum(weights)//4
+
+function group(cw, g_weights, weights)
+    cw == g && return g_weights
+
+    for w in weights
+        cw + w > g && continue
+        ws = copy(weights)
+        gw = copy(g_weights)
+        push!(gw, w)
+        ws = setdiff(ws, [w])
+        gw = group(cw + w, gw, ws)
+        gw != nothing && return gw
+    end
+end
+
+# Since we pick biggest weights first we know that 'group' returns a solution with the
+# smallest number of elements for a group of weight 'g'
+candidate = group(0, [], weights)
+
+# Now we have to figure out if there is another candidate of the same length with a lower
+# quantum entanglement
+qe = typemax(Int)
+
+for s in subsets(weights, length(candidate))
+    sqe = reduce(*, s)
+    (sum(s) != g || sqe >= qe) && continue
+
+    # Confirm that there is still a solution with this candidate for the other groups
+    w = setdiff(weights, candidate)
+    gw = group(0, [], w)
+    gw == nothing && continue
+    w = setdiff(w, gw)
+    gw = group(0, [], w)
+    gw == nothing && continue
+    @assert sum(setdiff(w, gw)) == g
+
+    canditate = s
+    qe = sqe
+end
+
+println(qe)
