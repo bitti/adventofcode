69fb4001a47ee1b3f2e48b03e2e0992f092ab769
David Ongaro <david.ongaro@hamburg.de>
2015-12-24 23:36:17 -0800

    Solution day 23
    Learnings:
    
    * Non-capture-groups can be useful to save some code
    * "Assignment form" function definition syntax
    * Inner Constructors
    * TCO is not supported (yet?) in
      Julia (https://github.com/JuliaLang/julia/issues/4964). (But I ended up
      using a loop anyway.)

---
+++ 23/day23-part2.jl
@@ -0,0 +1,54 @@
+const format = r"(\w+) (\w|[+-]\d+)(?:, ([+-]\d+))?"
+
+immutable Instruction
+    op::Symbol
+    r::Symbol
+    off::Int
+end
+
+Instruction(op::Symbol, off::Int, _::Void) = Instruction(op, :nothing, off)
+Instruction(op::Symbol, _::Void, off::Int) = Instruction(op, :nothing, off)
+Instruction(op::Symbol, r::Symbol, _::Void) = Instruction(op, r, 0)
+
+type Computer
+    r::Dict
+    pc::Int
+    
+    Computer() = new(Dict(:a => 1, :b => 0), 1)
+end
+
+function run!(c::Computer, instr::Array{Instruction})
+    while c.pc >= 1 && c.pc <= length(instr)
+        i = instr[c.pc]
+        off = 1
+        if i.op == :hlf
+            c.r[i.r] >>= 1
+        elseif i.op == :tpl
+            c.r[i.r] *= 3
+        elseif i.op == :inc
+            c.r[i.r] += 1
+        elseif i.op == :jmp
+            off = i.off
+        elseif i.op == :jie
+            if c.r[i.r] & 1 == 0
+                off = i.off
+            end
+        elseif i.op == :jio
+            if c.r[i.r] == 1
+                off = i.off
+            end
+        end
+        c.pc += off
+    end
+end
+
+function Base.parse(str::Void)
+    nothing
+end
+
+const c = Computer()
+run!(c, map(readlines(STDIN)) do line
+    Instruction(map(parse, match(format, line).captures)...)
+end)
+
+println(c.r[:b])
